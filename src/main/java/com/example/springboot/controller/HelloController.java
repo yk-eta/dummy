711ba3ff1ed357df5b996fca8ea1c4cd389e05ff
Yu <yyk@zenology-solution.com>
2022-08-26 07:32:42 +0800

    test mvn

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from TSC Demo with CodePipeline!";
+		return "Greetings from TSC Demo with CodePipeline and test build!";
 	}
 
 }
